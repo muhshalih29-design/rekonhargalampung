f4db71eb6a3658559e95d72fa7ca788a514f9b8f
Muhammad Shalih <muhammadshalih@MacBook-Air-Muhammad.local>
2026-04-13 21:13:20 +0700

    Adjust progress bar color to dark teal

--- dist/index.php
+++ dist/index.php
@@ -372,7 +372,7 @@ $progress_rows = array_values($base);
       .progress-fill.fill-hpb,
       .progress-fill.fill-hd,
       .progress-fill.fill-hkd {
-        background: #47a9b5;
+        background: #1f6f8b;
       }
       .progress-text {
         position: absolute;
@@ -392,7 +392,7 @@ $progress_rows = array_values($base);
       .fill-hpb,
       .fill-hd,
       .fill-hkd {
-        background: #47a9b5;
+        background: #1f6f8b;
       }
       .progress-fill::after { display: none; }
       .progress-table { min-width: 760px; }
